e574540aee6c1a5237a1590da22203b779eece44
Ash Wilson <smashwilson@gmail.com>
2017-05-27 19:51:34 -0400

    chown the /run directory

--- script/container/entrypoint.sh
+++ script/container/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-chown -R nginx:nginx /var/cache/nginx /var/tmp/nginx
+chown -R nginx:nginx /var/cache/nginx /var/tmp/nginx /run/nginx/
 chmod -R g+r /var/www/pushbot.party/ /var/www/azurefire.net/
 
 python /python/tls-up.py

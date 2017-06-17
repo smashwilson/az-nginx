522cb326107f89a6998d9b69d2aaf58b9b4c6c8e
Ash Wilson <smashwilson@gmail.com>
2017-06-17 10:52:22 -0400

    Fetch the GraphQL schema directly from GitHub

--- pushbot.party/scripts/update-schema.js
+++ pushbot.party/scripts/update-schema.js
@@ -1,26 +1,12 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const fetch = require('node-fetch')
-const {
-  buildClientSchema,
-  introspectionQuery,
-  printSchema,
-} = require('graphql/utilities')
-
 const paths = require('../config/paths')
-const SERVER = 'https://api.pushbot.party/graphql'
 
-console.log(introspectionQuery)
+const URL = 'https://github.com/smashwilson/pushbot/raw/master/scripts/api/schema.graphql'
 
-// Save JSON of full schema introspection for Babel Relay Plugin to use
-fetch(SERVER, {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({query: introspectionQuery}),
-}).then(res => res.json()).then(schemaJSON => {
-  const graphQLSchema = buildClientSchema(schemaJSON.data)
-  fs.writeFileSync(paths.schemaGraphQL, printSchema(graphQLSchema))
-}).catch(err => console.error(err))
+fetch(URL)
+.then(response => response.text())
+.then(text => {
+  fs.writeFileSync(paths.schemaGraphQL, text, {encoding: 'utf8'})
+})

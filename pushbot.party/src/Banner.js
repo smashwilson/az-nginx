f076326b8009ed0f840bc7f5f7f59da23b1c561c
Ash Wilson <smashwilson@gmail.com>
2017-06-17 15:10:02 -0400

    Start a Banner component.

---
+++ pushbot.party/src/Banner.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+export default class Banner extends Component {
+  static propTypes = {
+    username: PropTypes.string,
+    title: PropTypes.string
+  }
+
+  render () {
+    return <div />
+  }
+}

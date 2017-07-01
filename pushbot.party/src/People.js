c006005cd528a49bbb580943cd111c965acf3d33
Ash Wilson <smashwilson@gmail.com>
2017-07-01 13:39:30 -0400

    Render people list

--- pushbot.party/src/People.js
+++ pushbot.party/src/People.js
@@ -1,16 +1,125 @@
 import React, {Component} from 'react'
+import {QueryRenderer, graphql} from 'react-relay'
+import PropTypes from 'prop-types'
 
-export default class People extends Component {
+import {environment} from './Transport'
+
+class Person extends Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      presence: PropTypes.string.isRequired,
+      avatar: PropTypes.shape({
+        image32: PropTypes.string.isRequired
+      }).isRequired,
+      status: PropTypes.shape({
+        message: PropTypes.string.isRequired
+      }).isRequired
+    }).isRequired,
+    title: PropTypes.string.isRequired
+  }
+
   render () {
+    const {name, presence, avatar, status} = this.props.user
+    const avatarURL = avatar.image32
+    const presenceIcon = presence === 'ACTIVE' ? 'fa-dot-circle-o' : 'fa-circle-o'
+    const {message} = status
+
     return (
-      <div className='panel panel-info'>
-        <div className='panel-heading'>
-          <h3 className='panel-title'>People</h3>
-        </div>
-        <div className='panel-body'>
-          People tab
-        </div>
+      <div>
+        <i className={`fa ${presenceIcon}`} aria-hidden='true' />
+        <img href={avatarURL} />
+        <p className='name'>{name}</p>
+        <p className='title'>{this.props.title}</p>
+        <p className='status-message'>{message}</p>
       </div>
     )
+  }
+}
+
+export default class People extends Component {
+  constructor (props, context) {
+    super(props, context)
+    this.renderResult = this.renderResult.bind(this)
+  }
+
+  render () {
+    const query = graphql`
+      query PeopleQuery {
+        users {
+          all {
+            name
+            presence
+
+            avatar {
+              image32
+            }
+
+            status {
+              message
+              emoji
+            }
+          }
+        }
+
+        titles: documents(set: "title") {
+          all(criteria: {}) {
+            edges {
+              node {
+                text
+                subject
+              }
+            }
+          }
+        }
+      }
+    `
+
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={query}
+        render={this.renderResult} />
+    )
+  }
+
+  renderResult ({error, props}) {
+    if (error) {
+      return <div>{error.message}</div>
+    }
+
+    return (
+      <div>
+        <h3>Dramatis Personae</h3>
+        <ul className='list-group'>
+          {this.collateUsers(props).map(({user, title}) => {
+            return (
+              <li key={user.name} className='list-group-item'>
+                <Person user={user} title={title} />
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
+
+  collateUsers (props) {
+    if (!props) {
+      return []
+    }
+
+    const titlesByUsername = {}
+    for (const title of props.titles.all.edges) {
+      titlesByUsername[title.node.subject] = title.node.text
+    }
+
+    const userData = props.users.all.map(user => {
+      return {user, title: titlesByUsername[user.name] || ''}
+    })
+
+    userData.sort((a, b) => a.user.name.localeCompare(b.user.name))
+
+    return userData
   }
 }

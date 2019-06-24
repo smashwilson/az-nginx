a2292ba5ed054078efb3151e328a678dd2f52f42
Ash Wilson <smashwilson@github.com>
2019-06-24 15:53:22 -0400

    Look in /etc/ssl/az

--- script/container/entrypoint.sh
+++ script/container/entrypoint.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 chown -R nginx:nginx /var/cache/nginx /var/tmp/nginx /run/nginx/
 
 MISSING=no
-for TLS_FILE in /etc/ssl/dhparams.pem /etc/ssl/backend.azurefire.net/fullchain.pem /etc/ssl/backend.azurefire.net/privkey.pem; do
+for TLS_FILE in /etc/ssl/az/dhparams.pem /etc/ssl/az/backend.azurefire.net/fullchain.pem /etc/ssl/az/backend.azurefire.net/privkey.pem; do
   if [ ! -f "${TLS_FILE}" ]; then
     printf "Missing required TLS credential: %s\n" "${TLS_FILE}" >&2
     MISSING=yes
